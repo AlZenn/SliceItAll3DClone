7027f87c24c14b5f570827c9d74013a6dcfd8d54
Zeynep <creaxx.crea@gmail.com>
2023-03-23 23:00:46 +0300

    Kodlar
    code

---
+++ Assets/InputSystem/Gun.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using StarterAssets;
+using TMPro;
+using UnityEngine.Windows;
+
+public class Gun : MonoBehaviour
+{
+    private StarterAssetsInputs _input;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _input = transform.root.GetComponent<StarterAssetsInputs>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_input.shoot)
+        {
+            Shoot();
+            _input.shoot = false;
+        }
+
+    }
+    void Shoot()
+    {
+        Debug.Log("Shoot");
+    }
+
+}
